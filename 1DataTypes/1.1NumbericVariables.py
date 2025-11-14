00e3c92b95c6f208e31a01981d48e1589dd755df
Prathmesh Patil <ppatil1021@gmail.com>
2025-11-14 11:21:13 +0530

    Add numeric variables

---
+++ 1DataTypes/1.1NumbericVariables.py
@@ -0,0 +1,12 @@
+#Numberic Variables 
+i=5#integer 
+j=-23
+k=3.24#float
+l='Rahul'#string    
+m=23j#complex number 
+
+print(type(i))
+print(type(j))  
+print(type(k))
+print(type(l))
+print(type(m))
