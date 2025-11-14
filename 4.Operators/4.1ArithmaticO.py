808c139ba61994558516345f471a8d1b372f81f0
Prathmesh Patil <ppatil1021@gmail.com>
2025-11-14 12:11:01 +0530

    Add example for arithmetic operators

---
+++ 4.Operators/4.1ArithmaticO.py
@@ -0,0 +1,10 @@
+#Arithmatic Operators 
+a=10
+b=5
+print("Addition:",a+b)
+print("Substraction:",a-b)
+print("multipication:",a*b)
+print("Division:",a/b)
+print("flooor Division:",a//b)
+print("modulus:",a%b)
+print("Exponention:",a**b)
