53202ae7888d45bed28af5d1d3007acbd83047e7
Prathmesh Patil <ppatil1021@gmail.com>
2025-11-14 11:25:23 +0530

    Add string examples

---
+++ 1DataTypes/1.3String.py
@@ -0,0 +1,21 @@
+
+#String
+
+name ="learn with anviga ai 1738@3248)+_(#)"
+print(type(name))
+
+#In string Charactor to Unicode
+a="A"
+print(ord(a))
+#In string Unicode to Charector 
+b=80
+print(chr(b))
+
+#Indexing and Slicing in String 
+c="Prarhmeah patil"
+print(c[4])#indexing
+
+d="avigna Ai"
+print(d[0:6:1])#slicing
+print(d[8:10:1])#slicing   
+
