0032ec154fadad41002f965bb129b11dfdf37ffe
Prathmesh Patil <ppatil1021@gmail.com>
2025-11-14 12:12:22 +0530

    Add example for comparison operators

---
+++ 4.Operators/4.4ComparisonOperator.py
@@ -0,0 +1,16 @@
+#Comparison Operators
+e=12.1
+b=12
+print(e == b)
+print(e != b)
+print(e >b)
+print( e > b)
+print(23 > 23)
+print(23 >= 23)
+print(45<=45)
+
+print(ord("A"))
+print(ord("B"))
+
+print("A">"B")
+print("A"<"B")  
