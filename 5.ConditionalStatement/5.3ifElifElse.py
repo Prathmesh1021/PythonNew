109b8c474df145f812ff7c558c31fffe8ac34133
Prathmesh Patil <ppatil1021@gmail.com>
2025-11-14 12:14:41 +0530

    Add example for if-elif-else statement

---
+++ 5.ConditionalStatement/5.3ifElifElse.py
@@ -0,0 +1,7 @@
+money=int(input("Enter Your Money:"))
+if money==10:
+    print("you can by choclate")
+elif money==20:
+    print("you can by ice-cream with choclate")
+else:
+    print("you can by burger")      
