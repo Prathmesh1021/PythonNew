37a741025e5346f2b03e2c39ecb5985a2fa55b8b
Prathmesh Patil <ppatil1021@gmail.com>
2025-11-14 18:00:46 +0530

    ForLoopExamples

---
+++ 7.Loops/7.1ForLoop.py
@@ -0,0 +1,40 @@
+# Sample Example
+for i in range (23,34,2):
+    print(i)
+
+#Example 20-50
+for j in range (20,51,1):
+    print(j)
+    
+#Example 16-1
+for k in range(16,0,-1):
+    print(k)
+
+#Example -3to-15
+for a in range(-3,-16,-1):
+    print(a)
+    
+#lets print the table of 5
+for b in range(5,51,5):
+    print(b)
+
+#accept input from user and print the multiplication table 
+n=int(input("Which table you want"))
+
+for c in range(n,n*10+1,n):
+    print(c)
+    
+#To Print Using For Loop Charectors 
+v ="AvignaAi"
+
+for z in range(5):
+    print(v[z])
+
+#To print Using Lenght 
+w ="AvignaAi is the best company"
+print(len(w))
+for y in range(len(w)):
+    print(w[y])
+
+
+    
