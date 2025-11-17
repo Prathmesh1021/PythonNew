63c365db6a5a2cf453e7b29172ca432363cd28f6
Prathmesh Patil <ppatil1021@gmail.com>
2025-11-17 10:57:22 +0530

    Add And Or Not Examples

--- 4.Operators/4.5LogicalO.py
+++ 4.Operators/4.5LogicalO.py
@@ -1,5 +1,8 @@
 #Logical operators
 
 print(10 > 11 and 34 == 34 and 34 > 4 and 45 < 90)
+print(12 > 11 and 34 == 34 and 34 > 4 and 45 < 90)
 print(10 < 5 or 34 != 34 or 224>35 or 33>435 or 50>34)
+print(10 < 5 or 34 < 3 or 224>3543 or 33>435 or 50<34)
 print(not(10 < 5))  
+print(not(10 > 5))  
