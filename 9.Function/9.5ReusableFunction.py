283da7e9705a37b39a0452a6431cf5e8a62e6848
Prathmesh Patil <ppatil1021@gmail.com>
2025-11-17 15:30:43 +0530

    Code reusable in function example

---
+++ 9.Function/9.5ReusableFunction.py
@@ -0,0 +1,13 @@
+def pallindrome(st):
+    rev =""
+    for i in range(len(st)-1,-1,-1):
+        rev=rev+st[i]
+        
+    if rev == st:\
+        print(f"{st} is a pallindrome")
+    else:
+        print(f"{st} is not a pallindrome")
+        
+pallindrome("naman")
+pallindrome("sakshi")
+pallindrome("123321")
