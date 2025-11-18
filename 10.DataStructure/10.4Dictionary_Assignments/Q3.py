6cffad6af13b4ac0923dce67c99944674564afb0
Prathmesh Patil <ppatil1021@gmail.com>
2025-11-18 19:00:04 +0530

    ASSIGNMENTS FOR DICTIONARY

---
+++ 10.DataStructure/10.4Dictionary_Assignments/Q3.py
@@ -0,0 +1,11 @@
+#Q3-->Count the Frequency of each elements in a list 
+
+a=[1,1,1,2,2,2,3,3,3,4,4,5,5,6,6,6,7,8,9]
+
+d={}
+for i in a:
+    if i in d.keys():
+        d[i] +=1
+    else:
+        d[i] = 1
+print(d)
