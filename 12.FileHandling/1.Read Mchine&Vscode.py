82470028967936544c6020f0d0163b75f7a6d58d
Prathmesh Patil <ppatil1021@gmail.com>
2025-11-18 19:06:52 +0530

    File Handling Operations

---
+++ 12.FileHandling/1.Read Mchine&Vscode.py
@@ -0,0 +1,7 @@
+#Finding File On our Machine
+p = open(r'E:\entity freamwork code\many to many.txt')
+print(p.read())
+
+#finding File on the vscode 
+q=open('main.py')
+print(q.read())
---
+++ 12.FileHandling/2.Crud.py
@@ -0,0 +1,5 @@
+r=open("superman.txt",'a')
+
+r.write("and now i am apending some content inside the file ")
+
+r.close()
