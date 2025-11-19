75fe020f3e0ec15a5af29f78ac03acd2b9541b35
Prathmesh Patil <ppatil1021@gmail.com>
2025-11-19 18:31:08 +0530

    Accesing Attributes And Method

---
+++ 13.OOPS/13.1Class/2.AccesingAttributeAndMethod.py
@@ -0,0 +1,10 @@
+#Accesing Attributes And Methods 
+class Animal:
+    type="Cat"  #Attributes
+    
+    def sound(self):#method
+        print("Meow!")
+        
+#diretly accessing attribute and method using the class 
+print(Animal().type)
+Animal().sound()
