cb5ce1309bb84ecfa86280deae7a667fda183ea8
Prathmesh Patil <ppatil1021@gmail.com>
2025-11-19 18:30:01 +0530

    Class Example Using Attributes and Method

---
+++ 13.OOPS/13.1Class/1.ClassExample1.py
@@ -0,0 +1,8 @@
+#Class Example Using Attribute and method 
+class Factory:
+    a=12 #attribute
+    def hello(self):#method
+        print("how are you")
+print("hello how are you i am getting initialized ")
+print(Factory().a)
+Factory().hello()
