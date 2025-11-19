aa1a40761cce4888184b0bb9b126ff4d6ac77f2b
Prathmesh Patil <ppatil1021@gmail.com>
2025-11-19 18:32:05 +0530

    Add Types of Attributes

---
+++ 13.OOPS/13.1Class/3.AttributesTypes.py
@@ -0,0 +1,11 @@
+#There Are Class Attributes And Instance Attributes
+class car:
+    wheels=4#Class Attributes
+    def __init__(self,name):
+        self.name=name#instance attributes 
+    def show(self):
+        print(f"Welcome to {self.name}")
+        
+n=car("Creta")
+
+n.show()
