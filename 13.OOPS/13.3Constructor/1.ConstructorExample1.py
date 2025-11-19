239968e9b5d21e77993afc7bce80b65d842824fd
Prathmesh Patil <ppatil1021@gmail.com>
2025-11-19 18:33:25 +0530

    Add Constructor

---
+++ 13.OOPS/13.3Constructor/1.ConstructorExample1.py
@@ -0,0 +1,10 @@
+#Constructors
+class Students:
+    def __init__(self,name):
+        self.name=name #instance Attributes
+        
+#creating an object with a value 
+s=Students("Riya")
+
+#Accesing the attributes 
+print(s.name)
