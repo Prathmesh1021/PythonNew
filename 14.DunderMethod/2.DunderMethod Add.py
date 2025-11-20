67d6d00fa0e1504f378df034491981e92b89832a
Prathmesh Patil <ppatil1021@gmail.com>
2025-11-20 19:36:14 +0530

    Add Dunder method Addition of ages operation

---
+++ 14.DunderMethod/2.DunderMethod Add.py
@@ -0,0 +1,17 @@
+#In dunder method Addition of ages using objects 
+class Animal:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def __str__(self):
+        print("how are your and your name is {self.name}")
+    def __add__(self,other):
+        sum=0
+        for i in other:
+            sum=sum+i.age
+        return f"Your sum of ages are {self.age +sum}"
+obj=Animal("lion",12)
+obj2=Animal("dolphin",14)  
+obj3=Animal("tiger",34) 
+
+print(obj +(obj2,obj3))
