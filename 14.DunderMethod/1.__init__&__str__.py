a21764c47d2d0ffaffd0e7f9fa119827b0327325
Prathmesh Patil <ppatil1021@gmail.com>
2025-11-20 19:35:42 +0530

    Add Dunder method

---
+++ 14.DunderMethod/1.__init__&__str__.py
@@ -0,0 +1,8 @@
+# using Dunder Method 
+class Animal:
+    def __init__(self,name):
+        self.name =name
+    def __str__(self):
+        return f"hello how are you and your name is {self.name}"
+obj=Animal("lion")
+print(obj)
