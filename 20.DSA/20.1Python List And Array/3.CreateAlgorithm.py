ab2d7875ff3453b2437682bb3ba610f088f83cd9
Prathmesh Patil <ppatil1021@gmail.com>
2025-12-03 17:36:11 +0530

    Create Algorithm for find in list lowest number

---
+++ 20.DSA/20.1Python List And Array/3.CreateAlgorithm.py
@@ -0,0 +1,29 @@
+#Create an algorithm to find the lowest value in a list:
+my_array =[7,14,9,4,11,8]
+minVal =my_array[0]
+
+for i in my_array:
+    if i<minVal:
+        minVal=i
+        
+print('Lowest Value:',minVal)
+
+
+#####Practice 
+a=[3,5,6,2,7,47]
+b=a[0]
+
+for i in a:
+    if i<b:
+       b=i
+print("Lowest Value: ",b)
+
+
+#Create Algorithm Practice 
+z=[5,3,25,6,88,685]
+y=z[0]
+
+for i in z:
+    if i<y:
+      y=i
+print("Lowest Value:",y)
